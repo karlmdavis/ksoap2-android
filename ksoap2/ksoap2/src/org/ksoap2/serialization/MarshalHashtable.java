3de543fa0277c3b6093b429b50cd836971033e59
haustein <haustein@dcb2794c-2714-0410-ae40-8be703bc5849>
2004-03-04 10:27:51 +0000

    correct license now included in all source files
    git-svn-id: https://ksoap2.svn.sourceforge.net/svnroot/ksoap2/trunk@36 dcb2794c-2714-0410-ae40-8be703bc5849

--- ksoap2/ksoap2/src/org/ksoap2/serialization/MarshalHashtable.java
+++ ksoap2/ksoap2/src/org/ksoap2/serialization/MarshalHashtable.java
@@ -1,22 +1,28 @@
-/* kSOAP
+/* Copyright (c) 2003,2004, Stefan Haustein, Oberhausen, Rhld., Germany
  *
- * The contents of this file are subject to the Enhydra Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License
- * on the Enhydra web site ( http://www.enhydra.org/ ).
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
- * the License for the specific terms governing rights and limitations
- * under the License.
+ * The  above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * The Initial Developer of kSOAP is Stefan Haustein. Copyright (C)
- * 2000, 2001 Stefan Haustein, D-46045 Oberhausen (Rhld.),
- * Germany. All Rights Reserved.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE. 
  *
  * Contributor(s): Sean McDaniel
  *
  * */
+ 
+ 
 package org.ksoap2.serialization;
 
 import java.io.*;
