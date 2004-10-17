ff078fc0cdddb4a8358ccebb559ae5fe407616c1
haustein <haustein@dcb2794c-2714-0410-ae40-8be703bc5849>
2004-10-17 21:30:05 +0000

    sample by matthias wessendorf added
    git-svn-id: https://ksoap2.svn.sourceforge.net/svnroot/ksoap2/trunk@42 dcb2794c-2714-0410-ae40-8be703bc5849

---
+++ ksoap2/ksoap2/axis_sample/net/wessendorf/j2me/SoapDemo.java
@@ -0,0 +1,85 @@
+package net.wessendorf.j2me;
+
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.TextBox;
+import javax.microedition.lcdui.TextField;
+import javax.microedition.midlet.MIDlet;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.SoapFault;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransport;
+
+
+
+public class SoapDemo extends MIDlet implements CommandListener{
+    
+    private Command exitCommand;
+    private Display display;
+    
+    Form mainForm = new Form ("Hello World WebService");
+    TextField nameField = new TextField ("Your name","",456,TextField.ANY);
+    Command getCommand = new Command ("send", Command.SCREEN, 1);
+
+    public SoapDemo () {
+    	mainForm.append (nameField);
+    	mainForm.addCommand (getCommand);
+    	mainForm.setCommandListener (this);
+    }
+    
+    public void startApp() {
+        display = Display.getDisplay (this);
+        display.setCurrent (mainForm);
+  }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+    
+
+    public void commandAction(Command c, Displayable s) {
+        if (c == getCommand) {
+         	final TextBox t = new TextBox("", "", 256, 0);            
+        	Thread thr = new Thread(){
+        	    public void run() {    
+        	try {
+             
+                SoapObject client = new
+                SoapObject("","getObject");
+                client.addProperty("name",nameField.getString());
+                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+                envelope.bodyOut = client;
+                HttpTransport ht = new
+                HttpTransport("http://localhost:8080/axis/services/AxisService");
+                ht.call("", envelope);
+
+                SoapObject ret = new SoapObject("http://ws.wessendorf.net","CustomObject");
+                ret =(SoapObject)envelope.getResult();
+                t.setString(ret.getProperty(0).toString());
+            }
+            catch ( SoapFault sf){
+                String faultString = "Code: " + sf.faultcode + "\nString: " + sf.faultstring;
+                t.setString(faultString);
+            }
+            catch ( Exception e){
+                e.printStackTrace();
+                t.setString(e.getMessage());
+            }
+           	}};
+           	thr.start();
+            display.setCurrent(t);
+        }
+        else{
+            destroyApp(false);
+            notifyDestroyed();
+        }
+    }
+}
