0bcf87f04d2e18e03915c4f3c21f193731c6b108
Zachary Kurmas <zachary.kurmas@atomicobject.com>
2010-09-13 10:36:16 -0400

    First Attributes on Primitives test passes.

--- ksoap2-base/src/test/java/org/ksoap2/serialization/ParseTest.java
+++ ksoap2-base/src/test/java/org/ksoap2/serialization/ParseTest.java
@@ -1,9 +1,11 @@
 package org.ksoap2.serialization;
 
 import junit.framework.TestCase;
+import org.ksoap2.SoapEnvelope;
 import org.kxml2.io.KXmlParser;
 import org.xmlpull.v1.XmlPullParser;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 public class ParseTest extends TestCase {
@@ -16,20 +18,22 @@ public class ParseTest extends TestCase {
 
     public void testAttributesOnPrimitives() throws Exception
     {
-        String testXML = "<foo><bar name=\"fred\">Barney</bar></foo>";
-        InputStream inputStream = new IStringStream(textXML);
+        String testXML = "<body><response><result name=\"fred\" anotherAttr=\"anotherValue\">Barney</result></response></body>";
+        InputStream inputStream = new ByteArrayInputStream(testXML.getBytes());
+        SoapSerializationEnvelope sse = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+
 
        KXmlParser parser = new KXmlParser();
 		parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, true);
 		parser.setInput(inputStream, null);
-		parser.nextTag();
-		parser.nextTag();
-		parser.nextTag();
-		parser.nextTag();
-		return parser;
+        parser.nextTag();
 
+        sse.parseBody(parser);
 
-
+        SoapPrimitive bar = (SoapPrimitive) sse.getResponse();
+        assertEquals("fred", bar.safeGetAttribute("name"));
+        assertEquals("anotherValue", bar.safeGetAttribute("anotherAttr"));
+        assertEquals("Barney", bar.toString());
     }
 
 }
