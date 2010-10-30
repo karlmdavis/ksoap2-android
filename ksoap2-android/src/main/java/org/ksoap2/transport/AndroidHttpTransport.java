1f71252de4cff10c738ed0a7148ef24e9afc5adc
Manfred Moser (mosabua) <manfred@mosabuam.com>
2010-10-29 20:53:46 -0700

    deprecated empty placeholder classes, will remove whole module in future release most likely

--- ksoap2-android/src/main/java/org/ksoap2/transport/AndroidHttpTransport.java
+++ ksoap2-android/src/main/java/org/ksoap2/transport/AndroidHttpTransport.java
@@ -6,6 +6,8 @@ import java.io.IOException;
  * This is a simple extension of the {@link HttpTransportSE} class. It provides the exact same functionality
  * as that class and is provided purely for backwards-compatibility purposes. It will likely be deprecated at
  * some point in the near future.
+ *
+ * @deprecated use HttpTransportSE, this class will be removed in an upcoming release
  */
 public class AndroidHttpTransport extends HttpTransportSE
 {
--- ksoap2-android/src/main/java/org/ksoap2/transport/AndroidServiceConnection.java
+++ ksoap2-android/src/main/java/org/ksoap2/transport/AndroidServiceConnection.java
@@ -6,6 +6,8 @@ import java.io.IOException;
  * This is a simple extension of the {@link HttpTransportSE} class. It provides the exact same functionality
  * as that class and is provided purely for backwards-compatibility purposes. It will likely be deprecated at
  * some point in the near future.
+ *
+ * @deprecated use ServiceConnectionSE, this class will be remove in next release
  */
 public class AndroidServiceConnection extends ServiceConnectionSE
 {
