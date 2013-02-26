d98d21df408ae4e52a174dddd5bae61b12c8b8c8
Anatolii Shuba <x0158321@ti.com>
2013-02-26 22:37:15 +0200

    HttpsServiceConnectionSEIgnoringConnectionClose public modifier added
    Missed 'public' modifier has been added to the
    HttpsServiceConnectionSEIgnoringConnectionClose class definition

--- ksoap2-j2se/src/main/java/org/ksoap2/transport/HttpsServiceConnectionSEIgnoringConnectionClose.java
+++ ksoap2-j2se/src/main/java/org/ksoap2/transport/HttpsServiceConnectionSEIgnoringConnectionClose.java
@@ -2,7 +2,7 @@ package org.ksoap2.transport;
 
 import java.io.IOException;
 
-class HttpsServiceConnectionSEIgnoringConnectionClose extends HttpsServiceConnectionSE {
+public class HttpsServiceConnectionSEIgnoringConnectionClose extends HttpsServiceConnectionSE {
 
     public HttpsServiceConnectionSEIgnoringConnectionClose(String host, int port, String file, int timeout)
             throws IOException {
@@ -13,9 +13,7 @@ class HttpsServiceConnectionSEIgnoringConnectionClose extends HttpsServiceConnec
     public void setRequestProperty(String key, String value) {
         // We want to ignore any setting of "Connection: close" because
         // it is buggy with Android SSL.
-        if ("Connection".equalsIgnoreCase(key) && "close".equalsIgnoreCase(value)) {
-            return;
-        } else {
+        if (!"Connection".equalsIgnoreCase(key) && !"close".equalsIgnoreCase(value)) {
             super.setRequestProperty(key, value);
         }
     }
